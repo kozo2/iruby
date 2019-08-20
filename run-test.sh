164268abe91350c51bbea39cd770321b71b94869
Kozo Nishida <kozo.nishida@gmail.com>
2019-08-21 04:51:45 +0900

    Update run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -6,6 +6,9 @@ export PYTHON=python3
 
 ADAPTERS="cztop rbczmq ffi-rzmq pyzmq"
 
+python3 -m pip install nbconvert jupyter
+jupyter kernelspec list
+
 for adapter in $ADAPTERS; do
   export IRUBY_TEST_SESSION_ADAPTER_NAME=$adapter
   bundle exec rake test TESTOPTS=-v
